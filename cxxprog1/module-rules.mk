edfa6c95bd1fabc7c1c7f4f9e5297e4ef07f3343
nick <goodfella005@gmail.com>
2013-01-13 11:35:42 -0800

    Make cxxprog1 and cxxshlib1 prerequisites to their respective check targets:
    * This fixes errors when using Make's -j option.

--- cxxprog1/module-rules.mk
+++ cxxprog1/module-rules.mk
@@ -1,5 +1,6 @@
 check-prerule-files: $(call module_build_path,cxxprog1-prerule-file)
 check-cxxprog-flags: PARAMS += $(call module_build_path,cxxprog1),ATTRIBUTE_CFLAGS
+check-cxxprog-flags: $(call module_build_path,cxxprog1)
 
 $(call module_build_path,cxxprog1-prerule-file):
 	@touch $@
