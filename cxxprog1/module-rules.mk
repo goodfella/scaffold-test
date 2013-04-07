cf1ce522bfa661e64ed9bb17a4147453ee4beb89
nick <goodfella005@gmail.com>
2013-04-06 23:53:33 -0700

    Fix objs attribute support:
    * The source path should have been used.

--- cxxprog1/module-rules.mk
+++ cxxprog1/module-rules.mk
@@ -5,7 +5,7 @@ check-cxxprog-flags: $(call module_build_fullpath,cxxprog1)
 $(call module_build_fullpath,cxxprog1-prerule-file):
 	@touch $@
 
-$(call module_build_fullpath,cxxprog-obj.o): $(call module_source_fullpath,cxxprog-obj.cc)
+$(call module_source_fullpath,cxxprog-obj.o): $(call module_source_fullpath,cxxprog-obj.cc)
 	g++ -o $@ -c $<
 
 clean-prerule-files: FILES += $(call module_build_fullpath,cxxprog1-prerule-file)
--- cxxprog1/module-rules2.mk
+++ cxxprog1/module-rules2.mk
@@ -1,4 +1,4 @@
 clean: clean-cxxprog-obj.o
 .PHONY: clean-cxxprog-obj.o
-clean-cxxprog-obj.o: $(call module_build_fullpath,cxxprog-obj.o)
+clean-cxxprog-obj.o: $(call module_source_fullpath,cxxprog-obj.o)
 	rm -f $<
